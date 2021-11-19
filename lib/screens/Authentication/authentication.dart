0df75ccc7411b16e12eb8d0820be9ce430dbf3e5
prasad-791 <aniketkhalkar791@gmail.com>
2021-11-19 16:28:39 +0530

    Structure for App

---
+++ lib/screens/Authentication/authentication.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Authentication extends StatelessWidget {
+  const Authentication({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Text('Authentication'),
+      ),
+    );
+  }
+}
