9bfe62373d57c10487c622288910050648841fc7
Ruturaj Patil <ruturajp551@gmail.com>
2021-11-25 04:00:45 +0530

    State Management

---
+++ lib/screens/Splash/splashScreen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const routeName = '/splash';
+
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void Auth() async {
+    final pref = await SharedPreferences.getInstance();
+    var token = pref.getString('token');
+    if (token == null) {
+      Navigator.pushReplacementNamed(context, '/auth');
+    } else {
+      Navigator.pushReplacementNamed(context, '/nav');
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    Auth();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SpinKitPouringHourGlassRefined(
+      color: Color.fromARGB(255, 235, 60, 98),
+      size: 100,
+    );
+  }
+}
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,74 +1,85 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:music_recommender/models/users.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final String _url = "https://muzic-recommender.herokuapp.com";
 
 late User _currentUser;
 
-Future<User> registerUser(String email,String password,String username) async {
-    final response = await http.post(
-      Uri.parse(_url+'/user/signup'),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode(<String,String>{
-        "userName": username,
-        "email":  email,
-        "password": password
-      }),
-    );
-
-    if(response.statusCode == 201){
-      return User.fromJson(jsonDecode(response.body));
-    }else{
-      print(response.statusCode);
-      throw Exception(response.body);
-    }
-}
-
-Future<User> loginUser(String email,String password) async {
+Future<User> registerUser(
+    String email, String password, String username) async {
+  final pref = await SharedPreferences.getInstance();
   final response = await http.post(
-    Uri.parse(_url+'/user/login'),
+    Uri.parse(_url + '/user/signup'),
     headers: <String, String>{
       'Content-Type': 'application/json',
     },
-    body: jsonEncode(<String,String>{
-      "email":  email,
+    body: jsonEncode(<String, String>{
+      "userName": username,
+      "email": email,
       "password": password
     }),
   );
 
-  if(response.statusCode == 200){
-    return User.fromJson(jsonDecode(response.body));
-  }else{
+  if (response.statusCode == 201) {
+    User user = User.fromJson(jsonDecode(response.body));
+    pref.setString('token', user.token);
+    pref.setString('email', user.email);
+    pref.setString('userName', user.userName);
+    return user;
+  } else {
     print(response.statusCode);
     throw Exception(response.body);
   }
 }
 
-Future<bool> logOutUser()async{
+Future<User> loginUser(String email, String password) async {
+  final pref = await SharedPreferences.getInstance();
   final response = await http.post(
-    Uri.parse(_url+'/user/logout'),
+    Uri.parse(_url + '/user/login'),
     headers: <String, String>{
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+_currentUser.token,
+    },
+    body: jsonEncode(<String, String>{"email": email, "password": password}),
+  );
+
+  if (response.statusCode == 200) {
+    User user = User.fromJson(jsonDecode(response.body));
+    pref.setString('token', user.token);
+    pref.setString('email', user.email);
+    pref.setString('userName', user.userName);
+    return user;
+  } else {
+    print(response.statusCode);
+    throw Exception(response.body);
+  }
+}
+
+Future<bool> logOutUser() async {
+  final response = await http.post(
+    Uri.parse(_url + '/user/logout'),
+    headers: <String, String>{
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + _currentUser.token,
     },
   );
 
-  if(response.statusCode == 200){
+  if (response.statusCode == 200) {
+    final pref = await SharedPreferences.getInstance();
+    pref.remove('token');
+    pref.remove('email');
+    pref.remove('userName');
     return true;
-  }else{
+  } else {
     return false;
   }
-
-
 }
 
-User getUser(){
+User getUser() {
   return _currentUser;
 }
 
-void storeUser(User user){
+void storeUser(User user) {
   _currentUser = user;
 }
